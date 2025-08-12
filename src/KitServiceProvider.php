70547c26de56c7f1754c34ee544850b35a015db9
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-08-12 17:53:13 +0000

    Fix styling

--- src/KitServiceProvider.php
+++ src/KitServiceProvider.php
@@ -143,7 +143,7 @@ class KitServiceProvider extends PackageServiceProvider
                 /** @var \Illuminate\Routing\Route $this */
                 $uri = $this->uri();
                 $cleanUri = ltrim($uri, '/');
-                $actions = array_filter($this->getAction(), fn($key) => $key != 'as', ARRAY_FILTER_USE_KEY);
+                $actions = array_filter($this->getAction(), fn ($key) => $key != 'as', ARRAY_FILTER_USE_KEY);
                 FacadesRoute::addRoute(
                     $this->methods(),
                     '{lang}/' . $cleanUri,
@@ -247,6 +247,7 @@ class KitServiceProvider extends PackageServiceProvider
             'logo' => logo(),
         ];
         static::$viewShare = $data;
+
         return $data;
     }
 }
