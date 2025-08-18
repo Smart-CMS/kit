47420329423ef021742f66223d9fdaf1fe092fbc
maxboyko <maxboiko@divotek.com>
2025-08-18 20:41:47 +0300

    add page variables

--- src/Components/PageComponent.php
+++ src/Components/PageComponent.php
@@ -14,6 +14,7 @@ class PageComponent extends Component
     {
         app('template')->set(Template::query()->orderBy('sorting')->where('entity_type', Page::class)->where('entity_id', $page->id)->get());
         $seo = Seo::query()->where('seoable_id', $page->id)->where('seoable_type', Page::class)->first() ?? new Seo;
+        $page->seo = $seo;
         app('microdata')->add(BreadcrumbsMicrodata::run($page->getBreadcrumbs()));
         app('seo')->title($seo->title);
         app('seo')->description($seo->description);
--- src/Models/Front/FrontPage.php
+++ src/Models/Front/FrontPage.php
@@ -2,7 +2,10 @@
 
 namespace SmartCms\Kit\Models\Front;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use SmartCms\Kit\Models\Page;
+use SmartCms\Seo\Models\Seo;
 
 class FrontPage extends Page
 {
@@ -16,5 +19,80 @@ class FrontPage extends Page
     public function getCasts(): array
     {
         return array_merge(self::$staticCasts, $this->casts);
+    }
+
+    public function heading(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->getSeo()->heading ?? $this?->name ?? '',
+        );
+    }
+
+    public function summary(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->getSeo()->summary ?? $this?->name ?? '',
+        );
+    }
+
+    public function content(): Attribute
+    {
+        return new Attribute(
+            get: fn() => str($this->getSeo()->content ?? '')->toHtmlString(),
+        );
+    }
+
+    public function getSeo(): Seo
+    {
+        $seo = $this->seo ?? Seo::query()->where('seoable_id', $this->id)->where('seoable_type', Page::class)->first() ?? new Seo();
+        return $seo;
+    }
+
+    public function categories(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                if ($this->parent_id) {
+                    return FrontPage::query()->where('id', 0);
+                }
+                $settings = $this->settings ?? [];
+                $isCategories = $settings['is_categories'] ?? false;
+                if (!$isCategories) {
+                    return FrontPage::query()->where('id', 0);
+                }
+                return FrontPage::query()->where('root_id', $this->id)->where('parent_id', '!=', $this->id);
+            }
+        );
+    }
+
+    public function items(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                if ($this->parent_id) {
+                    return FrontPage::query()->where('id', 0);
+                }
+                $settings = $this->settings ?? [];
+                $isCategories = $settings['is_categories'] ?? false;
+                if (!$isCategories) {
+                    return FrontPage::query()->where('root_id', $this->id)->where('parent_id', $this->id);
+                }
+                return FrontPage::query()->where('root_id', $this->id)->where('parent_id', '!=', $this->id);
+            }
+        );
+    }
+
+    public function breadcrumbs(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->getBreadcrumbs(),
+        );
+    }
+
+    public function url(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->route(),
+        );
     }
 }
