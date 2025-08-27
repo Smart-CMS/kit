03c51187df69fa308ae481028e650ea6fe29d886
maxboyko <maxboiko@divotek.com>
2025-08-27 15:28:10 +0300

    Add error handling for admin notification in ContactFormObserver

--- src/Observers/ContactFormObserver.php
+++ src/Observers/ContactFormObserver.php
@@ -2,6 +2,7 @@
 
 namespace SmartCms\Kit\Observers;
 
+use Illuminate\Support\Facades\Log;
 use SmartCms\Forms\Models\ContactForm;
 use SmartCms\Kit\Models\Admin;
 use SmartCms\Kit\Notifications\NewContactFormNotification;
@@ -11,7 +12,16 @@ class ContactFormObserver
     public function saved(ContactForm $contactForm)
     {
         foreach (Admin::all() as $admin) {
-            $admin->notify(new NewContactFormNotification($contactForm));
+            try {
+                $admin->notify(new NewContactFormNotification($contactForm));
+            } catch (\Exception $e) {
+                Log::error('Error sending contact form notification to admin: ' . [
+                    'message' => $e->getMessage(),
+                    'admin' => $admin->id,
+                    'contact_form' => $contactForm->id,
+                    'trace' => 'ContactFormObserver.saved',
+                ]);
+            }
         }
     }
 }
